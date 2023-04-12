aaf650525cb1b5134f22013928adc6d5c92d9190
Sergey Merlov <sergei.merlow@ya.ru>
2023-04-12 23:05:11 +0300

    for commit

--- src/main/java/ru/msv/blog/blog/controller/BlogController.java
+++ src/main/java/ru/msv/blog/blog/controller/BlogController.java
@@ -55,4 +55,19 @@ public class BlogController {
         model.addAttribute("post", res);
         return "blog-edit";
     }
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUbdate(@PathVariable(value = "id") Long id,@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostDelete(@PathVariable(value = "id") Long id, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
 }
